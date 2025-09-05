987d4d9db0485a0f51b4a4e9390128d4723f0490
Pankaj Barakoti <pankajbarakoti837@gmail.com>
2025-09-05 15:33:04 +0530

    raiso2

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -7,7 +7,6 @@ import ScheduleVisit from "./ScheduleVisit";
 import FeaturesSection from "./FeaturesSection";
 import ChooseApartment from "./ChooseApartment";
 import NearbyPlaces from "./NearbyPlaces";
-import { AppWindow } from "lucide-react";
 import AppartmentPlans from "./AppartmentPlans";
 import PropertyFeatures from "./PropertyFeatures";
 import BlogSection from "./BlogSection";
